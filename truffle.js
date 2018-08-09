0f9d2487a03c2f52eca031c585702938bca7823f
Matt <sifrawr@gmail.com>
2018-08-09 14:57:25 +1200

    created truffle environment for tests to run. fixed import path for PrivateSaleRules.sol

---
+++ truffle.js
@@ -0,0 +1,31 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // eslint-disable-line camelcase
+      gas: 6700000
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*", // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    }
+};
